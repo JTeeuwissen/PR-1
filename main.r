8de603316dd08637b49782e55a5b9cccbf506b80
Rienk <rienkfidder@gmail.com>
2021-11-19 10:54:24 +0100

    Some simple statistics

--- main.r
+++ main.r
@@ -1,5 +1,23 @@
 library(OpenImageR)
+library(dplyr)
 
 # Assuming mnist.csv is located in the data folder, it's gitignored so make sure you download it
-mnist.dat <- read.csv("./data/mnist.csv")
-imageShow(matrix(as.numeric(mnist.dat[380,-1]),nrow=28,ncol=28,byrow=T))
+data <- read.csv("./data/mnist.csv")
+
+print_data_summary <- function() {
+    print("Labeled instances per class:")
+    data %>% count(label)
+
+    col_means <-colMeans(data)
+
+    print("Mean value per pixel:")
+    print(format(col_means, digits=1, scientific = FALSE))
+
+    print("50 least contributing pixels:")
+    print(sort(col_means)[0:50])
+
+    print("50 most varying pixels:")
+    sort(sapply(data, var), decreasing = TRUE)[0:50]
+}
+
+print_data_summary()
