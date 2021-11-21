85e82a4e0a9bcea0111c2c485b2a6e0fc84b9d24
Li-Yeun <45470843+Li-Yeun@users.noreply.github.com>
2021-11-21 22:55:36 +0100

    Converting label to a categorical variable

--- main.r
+++ main.r
@@ -3,12 +3,14 @@ library(dplyr)
 
 # Assuming mnist.csv is located in the data folder, it's gitignored so make sure you download it
 data <- read.csv("./data/mnist.csv")
+# Convert the label to a categorical variable.
+data$label <- as.factor(data$label)
 
 print_data_summary <- function() {
-    print("Labeled instances per class:")
-    data %>% count(label)
+    print("Class distribution:")
+    print(summary(data$label))
 
-    col_means <-colMeans(data)
+    col_means <-colMeans(data[, -1])
 
     print("Mean value per pixel:")
     print(format(col_means, digits=1, scientific = FALSE))
@@ -17,7 +19,7 @@ print_data_summary <- function() {
     print(sort(col_means)[0:50])
 
     print("50 most varying pixels:")
-    sort(sapply(data, var), decreasing = TRUE)[0:50]
+    print(sort(sapply(data[, -1], var), decreasing = TRUE)[0:50])
 }
 
 print_data_summary()
