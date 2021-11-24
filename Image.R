777b7628f32ff2a753419e28bb19165e321cd89d
Jelle Teeuwissen <j.teeuwissen@picasse.com>
2021-11-24 16:00:50 +0100

    Print image

---
+++ Image.R
@@ -0,0 +1,8 @@
+#' Print an image from a data frame segment
+#'
+#' @param data A data frame segment
+print_image <- function(data) {
+  matrix <- matrix(as.numeric(data), nrow = 28, ncol = 28)
+  image(matrix, col = grey(seq(1, 0, length = 256)), xaxt = "n", yaxt = "n")
+}
+print_image(data[380, 2:785])
