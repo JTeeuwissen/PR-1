4fd4490378068dae8db3c63c87b4155ecbec65ce
Jelle Teeuwissen <j.teeuwissen@picasse.com>
2021-11-24 15:28:38 +0100

    Cleaning

---
+++ Install.R
@@ -0,0 +1,2 @@
+install.packages("OpenImageR")
+install.packages("dplyr")
--- main.r
+++ main.r
@@ -1,25 +1,31 @@
 library(OpenImageR)
 library(dplyr)
 
-# Assuming mnist.csv is located in the data folder, it's gitignored so make sure you download it
-data <- read.csv("./data/mnist.csv")
+# Assuming mnist.csv is located in the data folder,
+# it's gitignored so make sure you download it
+data <- read.csv("mnist.csv")
+
 # Convert the label to a categorical variable.
 data$label <- as.factor(data$label)
+
 # Compute the total ink cost per sample.
 ink <- rowSums(data[-1])
+
 # Compute the mean value of ink for each digit.
 digits_ink_mean <- tapply(ink, data$label, mean)
+
 # Compute the standard deviation of ink cost for each digit.
 digits_ink_sd <- tapply(ink, data$label, sd)
 
+#' Print a summary of the data
 print_data_summary <- function() {
     print("Class distribution:")
     print(summary(data$label))
 
-    col_means <-colMeans(data[, -1])
+    col_means <- colMeans(data[, -1])
 
     print("Mean value per pixel:")
-    print(format(col_means, digits=1, scientific = FALSE))
+    print(format(col_means, digits = 1, scientific = FALSE))
 
     print("50 least contributing pixels:")
     print(sort(col_means)[0:50])
