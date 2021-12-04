950870d05fcce61054acd28b77af5dd41e6fe212
Li-Yeun <45470843+Li-Yeun@users.noreply.github.com>
2021-12-04 09:37:26 +0100

    tuning NN

--- Classifier/Neural Network.R
+++ Classifier/Neural Network.R
@@ -16,12 +16,11 @@ neural_network <- function(train_features,
   df <- as.data.frame(train_features)
   df$label <- train_labels
 
-  # Should work, does not
   # Hyperparameter tuning
-  train_nn <- e1071::tune.nnet(
-    train_features,
-    train_labels,
-    size = c(1, 30),
+  train_nn <- tune.nnet(
+    label ~ .,
+    data = df,
+    size = c(1,10),
     MaxNWts = 10360
   )
 
@@ -33,6 +32,9 @@ neural_network <- function(train_features,
   #   MaxNWts = 100000,
   #   maxit = 100
   # )
+  
+  # Debug
+  message(paste0("optimal size-param value: ", train_nn$best.parameters))
 
   # Make predictions on the test set.
   predicted_labels <- predict(
@@ -54,8 +56,9 @@ confusion_matrix <- neural_network(
   train_labels = train_labels,
   test_features = test_set,
   test_label = test_labels
+
 )
-print_confusion_matrix(confusion_matrix)
+#print_confusion_matrix(confusion_matrix)
 
 # using all cells low resolution
 confusion_matrix_low <- neural_network(
